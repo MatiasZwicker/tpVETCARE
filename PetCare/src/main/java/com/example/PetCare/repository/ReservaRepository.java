69ee30d9e56be64dfb6ee94f8747f17610ef88f7
matifl3 <matifelissia2205@gmail.com>
2026-06-03 14:19:49 -0300

    elimine clase viejas  -reserva-cuidador

--- PetCare/src/main/java/com/example/PetCare/repository/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.PetCare.repository;
-
-import com.example.PetCare.model.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
-}
